34885dc0add9502b3594e8ade263f1c0f32066d4
UNICO-AUS\lli <leon.li@unico.com.au>
2018-11-23 15:25:17 +1100

    Changed variable names in env. Added env detection by username.

--- database/database.js
+++ database/database.js
@@ -1,32 +1,35 @@
+const os = require('os');
+
 require('dotenv').config();
 
 var mysql = require('mysql');
+var dbc;
 
 // The variables in the following function are in /.env
-var dbc = mysql.createPool({
-    /**
-     * Dev details (for local development)
-     */
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
-    connectionLimit: process.env.DB_CONN,
-    debug: false,
-    multipleStatements: true
-
-    /**
-     * Prod details (for deploying on a hosting service)
-     */
-    // host: process.env.DB_HOST_PROD,
-    // port: process.env.DB_PORT_PROD,
-    // user: process.env.DB_USER_PROD,
-    // password: process.env.DB_PWD_PROD,
-    // database: process.env.DB_NAME_PROD,
-    // connectionLimit: process.env.DB_CONN_PROD,
-    // debug: false,
-    // multipleStatements: true
-});
+if(os.userInfo().username === 'foreverj') {
+    dbc = mysql.createPool({
+        // Production
+        host: process.env.DB_HOST_PROD,
+        port: process.env.DB_PORT_PROD,
+        user: process.env.DB_USER_PROD,
+        password: process.env.DB_PWD_PROD,
+        database: process.env.DB_NAME_PROD,
+        connectionLimit: process.env.DB_CONN_PROD,
+        debug: false,
+        multipleStatements: true
+    });
+} else {
+    dbc = mysql.createPool({
+        // Local development
+        host: process.env.DB_HOST_LOC,
+        port: process.env.DB_PORT_LOC,
+        user: process.env.DB_USER_LOC,
+        password: process.env.DB_PWD_LOC,
+        database: process.env.DB_NAME_LOC,
+        connectionLimit: process.env.DB_CONN_LOC,
+        debug: false,
+        multipleStatements: true
+    });
+}
 
 module.exports = dbc;
